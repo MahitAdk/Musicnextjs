ebe7f71ccd00762924031c5832c2805ff7112231
Mahit <mahitraj2004@gmail.com>
2025-08-06 21:03:10 +0545

    Navigation Bar Created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }){
+
+      const [active, setActive] = useState<string | null>(null);
+
+    return(
+        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+
+            <Menu setActive={setActive}>
+
+                <Link href={"/"}>
+                  
+                  <MenuItem setActive={setActive} active={active} item="Home">
+
+                  Home Page
+
+                  </MenuItem>
+
+                </Link>
+
+                
+
+                <MenuItem setActive={setActive} active={active} item="Our courses">
+                 <div className="flex flex-col space-y-4 text-sm">
+                  <HoveredLink href="/courses" className="text-black dark:text-white">
+                    All courses
+                  </HoveredLink>
+
+                  <HoveredLink href="/courses" className="text-black dark:text-white">
+                    Basic Music Theory
+                  </HoveredLink>
+
+                  <HoveredLink href="/courses" className="text-black dark:text-white">
+                    Advanced Composition
+                  </HoveredLink>
+
+                  <HoveredLink href="/courses" className="text-black dark:text-white">
+                    Songwriting
+                  </HoveredLink>
+
+                  <HoveredLink href="/courses" className="text-black dark:text-white">
+                    Music Production
+                  </HoveredLink>
+                 </div>
+
+                </MenuItem>
+
+                <Link href={"/Contact us"}>
+                  
+                  <MenuItem setActive={setActive} active={active} item="Contact Us">
+
+                  Contact Us
+
+                  </MenuItem>
+
+                </Link>
+
+
+            </Menu>
+
+        
+
+        </div>
+    )
+}
+
+export default Navbar
