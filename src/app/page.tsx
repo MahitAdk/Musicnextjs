5211ac93e5d36b1053ad632d8c5cffb1e2d32a8a
Mahit <mahitraj2004@gmail.com>
2025-08-11 15:10:38 +0545

    Changes Made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
     </div>
   );
 }
